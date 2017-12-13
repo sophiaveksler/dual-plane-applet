3e0650fa4a5a1de2b5b95f0712108bf27542de00
Sophia Veksler <sophia.veksler@wustl.edu>
2017-12-12 21:17:29 -0600

    part of it kind of works now idk

--- hamscript.js
+++ hamscript.js
@@ -3,20 +3,26 @@ var point;
 var drawingMode = true;
 
 var color = 0;
+var redSet = new Set();
+var blueSet = new Set();
 
 canvas.on('mouse:down', function (event) {
 	if (drawingMode) {
 		canvas.selection = false;
-		var x = event.e.clientX;
-		var y = event.e.clientY;
+		var offset = $('#ham-plane').offset();
+		var x = event.e.clientX - offset.left;
+		var y = event.e.clientY - offset.top;
 		if (color == 0) {
             point = new fabric.Circle({radius: 3, fill:'blue', left: x, top: y});
+            blueSet.add(point)
             color = 1;
         }
         else {
         	point = new fabric.Circle({radius: 3, fill:'red', left: x, top: y});
+        	redSet.add(point)
             color = 0;
         }
+        point.selectable = false;
     }
     });
 
