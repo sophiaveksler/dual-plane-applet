93e1ba079c75547fac324ac79dbdccd8d482ef92
Sophia Veksler <sophia.veksler@wustl.edu>
2017-12-15 15:54:27 -0600

    add to dual plane at same time

--- hamscript.js
+++ hamscript.js
@@ -17,17 +17,41 @@ canvas.on('mouse:down', function (event) {
 		var offset = $('#ham-plane').offset();
 		var x = event.e.clientX - offset.left;
 		var y = event.e.clientY - offset.top;
+
+		let colorName = 'blue';
 		if (color == 0) {
-            point = new fabric.Circle({radius: 3, fill:'blue', left: x, top: y, id: canvas.getObjects().length});
-            blueSet.add(point)
-            color = 1;
-        }
-        else {
-        	point = new fabric.Circle({radius: 3, fill:'red', left: x, top: y, id: canvas.getObjects().length});
-        	redSet.add(point)
-            color = 0;
-        }
+			color = 1;
+		}
+		else if (color == 1) {
+			colorName = 'red';
+			color = 0;
+		}
+
+
+       	point = new fabric.Circle({radius: 3, fill:colorName, left: x, top: y, id: canvas.getObjects().length});
+        redSet.add(point)
+       
         point.selectable = false;
+
+
+		let a = (point.left-(width/2.0))/50.0; // convert to more reasonable coordinates
+		let b = ((height - point.top) - (height/2.0))/50.0;
+
+		let x1 = 0;
+		let y1 = (((-width/2.0)/50.0)*a + b )*50 + width/2.0
+		let x2 = width;
+		let y2 = (((width/2.0)/50.0)*a + b )*50 + width/2.0;
+		//y = a*x - b; 
+
+		line = new fabric.Line([x1, y1, x2, y2],{
+			stroke: colorName,
+			fill: colorName, 
+			strokeWidth: 3,
+			id: point.id
+		})
+		line.selectable = false;
+		otherCanvas.add(line);
+
     }
     });
 
@@ -54,54 +78,54 @@ $('#reset').click(function () {
 })
 
 
-$('#dual-plane-conversion').click(function() {
+$('#ham-calculator').click(function() {
 	if (blueSet.size != redSet.size) { alert ("Add one more point please! You must have an even number of points to do the Ham Sandwich algorithm.")}
-	else {
-	otherCanvas.clear();
-	for (let p of blueSet) {
-		let a = (p.left-(width/2.0))/25.0; // convert to more reasonable coordinates
-		let b = ((height - p.top) - (height/2.0))/25.0;
+	// else {
+	// otherCanvas.clear();
+	// for (let p of blueSet) {
+	// 	let a = (p.left-(width/2.0))/50.0; // convert to more reasonable coordinates
+	// 	let b = ((height - p.top) - (height/2.0))/50.0;
 
-		let x1 = 0;
-		let y1 = (((-width/2.0)/25.0)*a + b )*25 + width/2.0
-		let x2 = width;
-		let y2 = (((width/2.0)/25.0)*a + b )*25 + width/2.0;
-		//y = a*x - b; 
+	// 	let x1 = 0;
+	// 	let y1 = (((-width/2.0)/50.0)*a + b )*50 + width/2.0
+	// 	let x2 = width;
+	// 	let y2 = (((width/2.0)/50.0)*a + b )*50 + width/2.0;
+	// 	//y = a*x - b; 
 
-		line = new fabric.Line([x1, y1, x2, y2],{
-			stroke: 'blue',
-			fill: 'blue', 
-			strokeWidth: 3,
-			id: p.id
-		})
-		line.selectable = false;
-		otherCanvas.add(line);
+	// 	line = new fabric.Line([x1, y1, x2, y2],{
+	// 		stroke: 'blue',
+	// 		fill: 'blue', 
+	// 		strokeWidth: 3,
+	// 		id: p.id
+	// 	})
+	// 	line.selectable = false;
+	// 	otherCanvas.add(line);
 
-	}
-	for (let p2 of redSet) {
-		let a = (p2.left-(width/2.0))/25.0;
-		let b = ((height - p2.top) - (height/2.0))/25.0 ;
+	// }
+	// for (let p2 of redSet) {
+	// 	let a = (p2.left-(width/2.0))/50.0;
+	// 	let b = ((height - p2.top) - (height/2.0))/50.0 ;
 
 
-		let x1 = 0;
-		let y1 = (((-width/2.0)/25.0)*a + b )*25 + width/2.0
-		let x2 = width;
-		let y2 = (((width/2.0)/25.0)*a + b )*25 + width/2.0;
-		//y = a*x - b; 
+	// 	let x1 = 0;
+	// 	let y1 = (((-width/2.0)/50.0)*a + b )*50 + width/2.0
+	// 	let x2 = width;
+	// 	let y2 = (((width/2.0)/50.0)*a + b )*50 + width/2.0;
+	// 	//y = a*x - b; 
 
-		line = new fabric.Line([x1, y1, x2, y2],{
-			stroke: 'red',
-			fill: 'red', 
-			strokeWidth: 3,
-			id: p2.id
-		})
-		line.selectable = false;
-		//line.selection = false; 
-		otherCanvas.add(line);
+	// 	line = new fabric.Line([x1, y1, x2, y2],{
+	// 		stroke: 'red',
+	// 		fill: 'red', 
+	// 		strokeWidth: 3,
+	// 		id: p2.id
+	// 	})
+	// 	line.selectable = false;
+	// 	//line.selection = false; 
+	// 	otherCanvas.add(line);
 
-	}
+	// }
 }
 
-})
+)
 
 
